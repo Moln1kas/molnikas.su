4c178ebf4c213ddf367b85b48a888a5585ff3ad3
Moln1kas <moln1qkas@gmail.com>
2024-09-08 22:47:07 +0300

    small fix

--- public/scripts/apps/indev.js
+++ public/scripts/apps/indev.js
@@ -2,7 +2,7 @@ class InDevApp {
     init() {
         $("#indev__title").html("В разработке");
         $("#indev__content").html(`
-            <p>Следите за разработкой в моем <a href="https://github.com/Moln1kas/molnikas.su">Github репозитории</a></p>
+            <p>Следите за разработкой в моем <a target="_blank" href="https://github.com/Moln1kas/molnikas.su">Github репозитории</a></p>
         `);
     }
 }
